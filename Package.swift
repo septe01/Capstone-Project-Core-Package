b2966289df2aa5e8a7664053e1b3e0d872367d37
septe habudin <septe.vhirgo@gmail.com>
2023-01-09 08:24:07 +0700

    update

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CPCorePackage",
-            targets: ["CPCorePackage"]),
+            targets: ["CPCorePackage"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/Alamofire/Alamofire.git", branch: "master"),
-//        .package(url: "https://github.com/realm/realm-swift", branch: "master")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", branch: "master"),
+        .package(url: "https://github.com/realm/realm-swift", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
